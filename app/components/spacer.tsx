db0aeec5708ab27469c94f689fa37ba1d4be7c8c
innocentdavid <paulinnocent04@gmail.com>
2023-12-09 21:15:32 +0100

    st

--- app/components/spacer.tsx
+++ app/components/spacer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Spacer() {
   return (
-    <div className='min-h-[80px]'></div>
+    <div className='min-h-[120px]'></div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,47 @@ import SampleQuestionsSlider from "./components/home/sampleQuestionsSlider";
 import Nav from "./components/layout/nav";
 
 const testimonials = [
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-  { title: 'Best site I’ve ever been on', desc: 'Very knowledgeable about any question, and they answer within minutes.', user: 'Kelvin' },
-]
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+  {
+    title: "Best site I’ve ever been on",
+    desc: "Very knowledgeable about any question, and they answer within minutes.",
+    user: "Kelvin",
+  },
+];
 
 export default function Home() {
   return (
@@ -88,6 +120,153 @@ export default function Home() {
 
         <Spacer />
 
+        <section id="services">
+          <div className="flex flex-col items-center">
+            <h2 className="font-PoppinsBold text-[60px] font-bold leading-[60px] tracking-[1px] text-primary/10 uppercase">
+              Testimonials
+            </h2>
+            <h3 className="text-[48px] font-bold -mt-11">
+              What our clients say
+            </h3>
+            <div className="h-1 bg-primary min-w-[100px]"></div>
+          </div>
+
+          <div className="mt-12"></div>
+
+          <div className="flex justify-center">
+            <div className="flex flex-col gap-12">
+              <div className="h-[135px] flex items-center">
+                <h2 className="font-PoppinsBold text-[60px] font-bold leading-[60px] tracking-[1px] text-primary/10 uppercase mr-7">
+                  01
+                </h2>
+                <Image
+                  src={"circle-check.svg"}
+                  alt=""
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <div className="min-w-[210px] border border-dashed"></div>
+                <div className="w-4 h-4 rounded-full -mr-[10px] bg-primary relative z-10"></div>
+              </div>
+
+              <div className="h-[135px] flex flex-col justify-center">
+                <h3 className="font-PoppinsBold font-bold text-[28px] text-primary ">
+                  Available 24/7
+                </h3>
+                <p className="mt-2 text-xl max-w-[400px]">
+                  Available 24/7 for your convenience! Get in touch anytime, day
+                  or night, for swift and reliable assistance.
+                </p>
+              </div>
+
+              <div className="h-[135px] flex items-center">
+                <h2 className="font-PoppinsBold text-[60px] font-bold leading-[60px] tracking-[1px] text-primary/10 uppercase mr-7">
+                  01
+                </h2>
+                <Image
+                  src={"circle-check.svg"}
+                  alt=""
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <div className="min-w-[210px] border border-dashed"></div>
+                <div className="w-4 h-4 rounded-full -mr-[10px] bg-primary relative z-10"></div>
+              </div>
+
+              <div className="h-[135px] flex flex-col justify-center">
+                <h3 className="font-PoppinsBold font-bold text-[28px] text-primary ">
+                  Available 24/7
+                </h3>
+                <p className="mt-2 text-xl max-w-[400px]">
+                  Available 24/7 for your convenience! Get in touch anytime, day
+                  or night, for swift and reliable assistance.
+                </p>
+              </div>
+
+              <div className="h-[135px] flex items-center">
+                <h2 className="font-PoppinsBold text-[60px] font-bold leading-[60px] tracking-[1px] text-primary/10 uppercase mr-7">
+                  01
+                </h2>
+                <Image
+                  src={"circle-check.svg"}
+                  alt=""
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <div className="min-w-[210px] border border-dashed"></div>
+                <div className="w-4 h-4 rounded-full -mr-[10px] bg-primary relative z-10"></div>
+              </div>
+            </div>
+            <div className="border border-dashed h-[calc(870px-130px)] mt-[65px]"></div>
+            <div className="flex flex-col gap-12">
+              <div className="h-[135px] flex flex-col justify-center px-8">
+                <h3 className="font-PoppinsBold font-bold text-[28px] text-primary">
+                  Expert lawyers
+                </h3>
+                <p className="mt-2 text-xl max-w-[400px]">
+                  We have high quality verified expert lawyers ready to help
+                  you.
+                </p>
+              </div>
+
+              <div className="h-[135px] flex items-center">
+                <div className="w-4 h-4 rounded-full -ml-[10px] bg-primary relative z-10"></div>
+                <div className="min-w-[210px] border border-dashed"></div>
+                <Image
+                  src={"circle-check.svg"}
+                  alt=""
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <h2 className="font-PoppinsBold text-[60px] font-bold leading-[60px] tracking-[1px] text-primary/10 uppercase ml-7">
+                  02
+                </h2>
+              </div>
+
+              <div className="h-[135px] flex flex-col justify-center px-8">
+                <h3 className="font-PoppinsBold font-bold text-[28px] text-primary">
+                  Expert lawyers
+                </h3>
+                <p className="mt-2 text-xl max-w-[400px]">
+                  We have high quality verified expert lawyers ready to help
+                  you.
+                </p>
+              </div>
+
+              <div className="h-[135px] flex items-center">
+                <div className="w-4 h-4 rounded-full -ml-[10px] bg-primary relative z-10"></div>
+                <div className="min-w-[210px] border border-dashed"></div>
+                <Image
+                  src={"circle-check.svg"}
+                  alt=""
+                  width={100}
+                  height={100}
+                  className=""
+                />
+                <h2 className="font-PoppinsBold text-[60px] font-bold leading-[60px] tracking-[1px] text-primary/10 uppercase ml-7">
+                  02
+                </h2>
+              </div>
+
+              <div className="h-[135px] flex flex-col justify-center px-8">
+                <h3 className="font-PoppinsBold font-bold text-[28px] text-primary">
+                  Expert lawyers
+                </h3>
+                <p className="mt-2 text-xl max-w-[400px]">
+                  We have high quality verified expert lawyers ready to help
+                  you.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <Spacer />
+
         <section
           id="cta"
           className="w-full h-[400px] bg-[url(/get-started-bg.svg)] bg-cover bg-center flex flex-col items-center justify-center gap-8 text-white"
@@ -100,7 +279,7 @@ export default function Home() {
             the answers you need now.
           </p>
           <Button
-            className="mt-3 text-primary bg-white rounded-[10px]"
+            className="mt-3 text-primary bg-white rounded-[10px] border-none"
             size="lg"
           >
             Get started
@@ -122,7 +301,10 @@ export default function Home() {
 
           <div className="mt-[100px] lg:grid grid-cols-4 gap-8 px-[60px]">
             {testimonials.map((testomial, index) => (
-              <div key={`testimonials_t-${index}`} className="shadow-[0px_0px_17.7px_3px_rgba(0,_0,_0,_0.09)] rounded-3xl w-[300px] h-[200px] bg-white px-[14px] py-[18px]">
+              <div
+                key={`testimonials_t-${index}`}
+                className="shadow-[0px_0px_17.7px_3px_rgba(0,_0,_0,_0.09)] rounded-3xl w-[300px] h-[200px] bg-white px-[14px] py-[18px]"
+              >
                 <div className="flex items-center gap-3">
                   <div className="">
                     <Image
@@ -141,9 +323,7 @@ export default function Home() {
                 </h4>
                 <div className="flex items-end">
                   <div className="">
-                    <p className="text-center">
-                      {testomial.desc}
-                    </p>
+                    <p className="text-center">{testomial.desc}</p>
                   </div>
 
                   <Image
@@ -160,11 +340,6 @@ export default function Home() {
         </section>
 
         <Spacer />
-        <Spacer />
-        <Spacer />
-
-
-
       </main>
     </>
   );
