e7181b1429a083f452d03e1ca4be78d642d583ec
innocentdavid <paulinnocent04@gmail.com>
2023-12-16 01:10:23 +0100

    api

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,87 +1,87 @@
 import { NextRequest, NextResponse } from "next/server";
-import NodeMailer from "nodemailer";
+// import NodeMailer from "nodemailer";
 
 export async function GET() {
   return NextResponse.json({ name: "Paul Innocent" });
 }
 
-const transporter = NodeMailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  debug: true,
-  auth: {
-    user: process.env.SMTP_LOGIN,
-    pass: process.env.SMTP_KEY,
-  },
-});
+// const transporter = NodeMailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: process.env.SMTP_PORT,
+//   debug: true,
+//   auth: {
+//     user: process.env.SMTP_LOGIN,
+//     pass: process.env.SMTP_KEY,
+//   },
+// });
 
-// const send_email = async (from, to, subject, content) => {
-//     var res;
-//     while (!res) {
+// // const send_email = async (from, to, subject, content) => {
+// //     var res;
+// //     while (!res) {
 
+// //     }
+// //     await transporter.sendMail(
+// //         { from: from, to, subject, html: content, sender: { name: "Aralingual", email: "contact@aralingual.com" }, },
+// //         (error, info) => {
+// //             if (error) {
+// //                 console.log(error);
+// //                 res = { success: false, message: error }
+// //             } else {
+// //                 // console.log("email sent to: " + info.accepted[0]);
+// //                 res ={ success: true, message: info.response }
+// //             }
+// //         }
+// //     )
+// //     return res
+// // }
+
+// const send_email = async (
+//   from: string,
+//   to: string,
+//   subject: string,
+//   content: string
+// ) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const info = await transporter.sendMail({
+//         from: from,
+//         to: to,
+//         subject: subject,
+//         html: content,
+//         // sender: { name: 'Aralingual', email: 'contact@aralingual.com' },
+//       });
+
+//       // If sending email is successful, resolve the promise with success message
+//       resolve({ success: true, message: info.response });
+//     } catch (error: any) {
+//       // If there's an error sending the email, reject the promise with the error
+//       reject({ success: false, message: error.message });
 //     }
-//     await transporter.sendMail(
-//         { from: from, to, subject, html: content, sender: { name: "Aralingual", email: "contact@aralingual.com" }, },
-//         (error, info) => {
-//             if (error) {
-//                 console.log(error);
-//                 res = { success: false, message: error }
-//             } else {
-//                 // console.log("email sent to: " + info.accepted[0]);
-//                 res ={ success: true, message: info.response }
-//             }
+//   });
+// };
+
+// export async function POST(request: NextRequest) {
+//   const { from, to, subject, content } = await request.json();
+//   try {
+//     const re: any = await send_email(from, to, subject, content).catch(
+//       (err) => {
+//         if (!err.success) {
+//           return { status: "error", message: err.message };
 //         }
-//     )
-//     return res
+//       }
+//     );
+//     if (!re.success) {
+//       return NextResponse.json(
+//         { status: "error", message: re.message },
+//         { status: 500 }
+//       );
+//     } else {
+//       return NextResponse.json({ status: "success" });
+//     }
+//   } catch (error: any) {
+//     return NextResponse.json(
+//       { status: "error", message: error.message },
+//       { status: 500 }
+//     );
+//   }
 // }
-
-const send_email = async (
-  from: string,
-  to: string,
-  subject: string,
-  content: string
-) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const info = await transporter.sendMail({
-        from: from,
-        to: to,
-        subject: subject,
-        html: content,
-        // sender: { name: 'Aralingual', email: 'contact@aralingual.com' },
-      });
-
-      // If sending email is successful, resolve the promise with success message
-      resolve({ success: true, message: info.response });
-    } catch (error: any) {
-      // If there's an error sending the email, reject the promise with the error
-      reject({ success: false, message: error.message });
-    }
-  });
-};
-
-export async function POST(request: NextRequest) {
-  const { from, to, subject, content } = await request.json();
-  try {
-    const re: any = await send_email(from, to, subject, content).catch(
-      (err) => {
-        if (!err.success) {
-          return { status: "error", message: err.message };
-        }
-      }
-    );
-    if (!re.success) {
-      return NextResponse.json(
-        { status: "error", message: re.message },
-        { status: 500 }
-      );
-    } else {
-      return NextResponse.json({ status: "success" });
-    }
-  } catch (error: any) {
-    return NextResponse.json(
-      { status: "error", message: error.message },
-      { status: 500 }
-    );
-  }
-}
--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -1,67 +1,67 @@
 import { NextResponse } from "next/server";
-import { fetchAPI } from '../../utils/fetch-api'
+// import { fetchAPI } from '../../utils/fetch-api'
 
-export async function GET() {
-    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-    const path = `/articles`;
-    const urlParamsObject = {
-        sort: { id: "asc" },
-        populate: '*',
-        pagination: {
-            start: 0,
-            limit: 999999,
-        },
-    };
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-    const res = await fetchAPI(path, urlParamsObject, options);
+// export async function GET() {
+//     const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+//     const path = `/articles`;
+//     const urlParamsObject = {
+//         sort: { id: "asc" },
+//         populate: '*',
+//         pagination: {
+//             start: 0,
+//             limit: 999999,
+//         },
+//     };
+//     const options = { headers: { Authorization: `Bearer ${token}` } };
+//     const res = await fetchAPI(path, urlParamsObject, options);
 
-    if (!res?.data) {
-        console.log("Failed to fetch data: => ", res.error.message);
-    }
+//     if (!res?.data) {
+//         console.log("Failed to fetch data: => ", res.error.message);
+//     }
 
-    const posts = res?.data
+//     const posts = res?.data
 
-    const maps = [
-        {
-            url: 'https://askus.vercel.app',
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        },
-        {
-            url: 'https://askus.vercel.app/about-us',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: 'https://askus.vercel.app/faq',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: 'https://askus.vercel.app/contact-us',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: 'https://aaskus.vercel.app/blog',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-        },
-    ]
+//     const maps = [
+//         {
+//             url: 'https://askus.vercel.app',
+//             lastModified: new Date(),
+//             changeFrequency: 'yearly',
+//             priority: 1,
+//         },
+//         {
+//             url: 'https://askus.vercel.app/about-us',
+//             lastModified: new Date(),
+//             changeFrequency: 'monthly',
+//             priority: 0.8,
+//         },
+//         {
+//             url: 'https://askus.vercel.app/faq',
+//             lastModified: new Date(),
+//             changeFrequency: 'monthly',
+//             priority: 0.8,
+//         },
+//         {
+//             url: 'https://askus.vercel.app/contact-us',
+//             lastModified: new Date(),
+//             changeFrequency: 'monthly',
+//             priority: 0.8,
+//         },
+//         {
+//             url: 'https://aaskus.vercel.app/blog',
+//             lastModified: new Date(),
+//             changeFrequency: 'weekly',
+//             priority: 0.5,
+//         },
+//     ]
 
-    posts.forEach((post: any) => {
-        maps.push({
-            url: `https://askus.vercel.app/${post.attributes.slug}`,
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        })
-    });
+//     posts.forEach((post: any) => {
+//         maps.push({
+//             url: `https://askus.vercel.app/${post.attributes.slug}`,
+//             lastModified: new Date(),
+//             changeFrequency: 'yearly',
+//             priority: 1,
+//         })
+//     });
 
-    return NextResponse.json(maps)
-}
+//     return NextResponse.json(maps)
+// }
