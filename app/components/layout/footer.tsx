72e9d332c707558c590bc53fc019af5e6f80b503
innocentdavid <paulinnocent04@gmail.com>
2023-12-16 02:59:29 +0100

    s

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,121 +1,124 @@
-import { Button, Image} from '@nextui-org/react'
-import Link from 'next/link'
-import React from 'react'
+'use client'
+
+import { Button, Image } from "@nextui-org/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function Footer() {
+  const pathname = usePathname()
+
   return (
-    <footer className="flex flex-col bg-[#0580FE] w-full lg:px-[60px] px-[5%]">
-              <div className="flex flex-col lg:flex-row justify-between w-full">
-                <div className="flex flex-col lg:items-start md:items-center">
-                  <Image
-                    src="/footer.svg"
-                    alt=""
-                    width={132}
-                    height={29}
-                    className="pt-[30px]"
-                  />
-                  <h1 className="text-[#FFF] lg:mt-[29px] mt-[18px] md:w-[400px] lg:font-PoppinsBold font-PoppinsSemiBold lg:font-[700] lg:tracking-[1px] lg:leading-[50.3px] text-[24px] md:text-center pr-4 md:pr-0 lg:text-start">
-                    If you have a question, AskUs
-                  </h1>
+    <footer className={`${pathname === '/profile' && 'mb-16 lg:mb-0'} flex flex-col bg-[#0580FE] w-full lg:px-[60px] px-[5%]`}>
+      <div className="flex flex-col lg:flex-row justify-between w-full">
+        <div className="flex flex-col lg:items-start md:items-center">
+          <Image
+            src="/footer.svg"
+            alt=""
+            width={132}
+            height={29}
+            className="pt-[30px]"
+          />
+          <h1 className="text-[#FFF] lg:mt-[29px] mt-[18px] md:w-[400px] lg:font-PoppinsBold font-PoppinsSemiBold lg:font-[700] lg:tracking-[1px] lg:leading-[50.3px] text-[24px] md:text-center pr-4 md:pr-0 lg:text-start">
+            If you have a question, AskUs
+          </h1>
 
-                  <Button
-                    className="lg:mt-[35px] mt-[20px] text-primary max-w-[175px] w-full h-[50px] bg-white rounded-[10px]"
-                    size={"lg"}
-                  >
-                    Get started
-                  </Button>
-                </div>
+          <Button
+            className="lg:mt-[35px] mt-[20px] text-primary max-w-[175px] w-full h-[50px] bg-white rounded-[10px]"
+            size={"lg"}
+          >
+            Get started
+          </Button>
+        </div>
 
-                <div className="flex flex-col md:flex-row justify-around gap-8 xl:gap-[100px] mt-[38px]">
-                  <div className="flex flex-col  ">
-                    <Link
-                      href="#"
-                      className="lg:text-[24px] text-[20px] font-PoppinsBold font-[700] xl:leading-[50.3px] leading-[30px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      General
-                    </Link>
-                    <Link
-                      href="/"
-                      className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      Home
-                    </Link>
-                    <Link
-                      href="/about"
-                      className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      About
-                    </Link>
-                  </div>
+        <div className="flex flex-col md:flex-row justify-around gap-8 xl:gap-[100px] mt-[38px]">
+          <div className="flex flex-col  ">
+            <Link
+              href="#"
+              className="lg:text-[24px] text-[20px] font-PoppinsBold font-[700] xl:leading-[50.3px] leading-[30px] tracking-[1px] text-[#FFFFFF]"
+            >
+              General
+            </Link>
+            <Link
+              href="/"
+              className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              About
+            </Link>
+          </div>
 
-                  <div className="flex flex-col ">
-                    <Link
-                      href="#"
-                      className="lg:text-[24px] text-[20px] font-PoppinsBold font-[700] xl:leading-[50.3px] leading-[30px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      How to Subscribe
-                    </Link>
-                    <Link
-                      href="/#why-ask-us"
-                      className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      Why use AskUs
-                    </Link>
-                    <Link
-                      href="/signup"
-                      className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      Sign Up
-                    </Link>
-                    <Link
-                      href="login"
-                      className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      Subscribers Login
-                    </Link>
-                  </div>
+          <div className="flex flex-col ">
+            <Link
+              href="#"
+              className="lg:text-[24px] text-[20px] font-PoppinsBold font-[700] xl:leading-[50.3px] leading-[30px] tracking-[1px] text-[#FFFFFF]"
+            >
+              How to Subscribe
+            </Link>
+            <Link
+              href="/#why-ask-us"
+              className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              Why use AskUs
+            </Link>
+            <Link
+              href="/signup"
+              className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              Sign Up
+            </Link>
+            <Link
+              href="login"
+              className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              Subscribers Login
+            </Link>
+          </div>
 
-                  <div className="flex flex-col ">
-                    <Link
-                      href="#"
-                      className="lg:text-[24px] text-[20px] font-PoppinsBold font-[700] xl:leading-[50.3px] leading-[30px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      Help
-                    </Link>
-                    <Link
-                      href="faq"
-                      className="text-[18px] font-Poppins  leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      FAQ
-                    </Link>
-                    <Link
-                      href="contact-us"
-                      className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
-                    >
-                      Contact Us
-                    </Link>
-                  </div>
-                </div>
-              </div>
+          <div className="flex flex-col ">
+            <Link
+              href="#"
+              className="lg:text-[24px] text-[20px] font-PoppinsBold font-[700] xl:leading-[50.3px] leading-[30px] tracking-[1px] text-[#FFFFFF]"
+            >
+              Help
+            </Link>
+            <Link
+              href="faq"
+              className="text-[18px] font-Poppins  leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              FAQ
+            </Link>
+            <Link
+              href="contact-us"
+              className="lg:text-[18px] text-[16px] font-Poppins font-[400] leading-[50.3px] tracking-[1px] text-[#FFFFFF]"
+            >
+              Contact Us
+            </Link>
+          </div>
+        </div>
+      </div>
 
-              <div className="mt-8 flex items-center justify-end w-full h-full mb-6 flex-col text-[18px] font-Poppins leading-[50.3px] tracking-[1px] text-[#FFFFFF]">
-                <div className="">
-                  ©2023 AskUs Limited. All rights reserved.
-                </div>
-                <div className="flex items-center gap-3">
-                  <Link href="/privacy-policy" className="text-[#FFFFFF]">
-                    Privacy Policy
-                  </Link>
-                  <div className=""> | </div>
-                  <Link href="/terms-of-service" className="text-[#FFFFFF]">
-                    Terms of Service
-                  </Link>
-                  <div className=""> | </div>
-                  <Link href="/sitemap" className="text-[#FFFFFF]">
-                    Sitemap
-                  </Link>
-                </div>
-              </div>
-            </footer>
-  )
+      <div className="mt-8 flex items-center justify-end w-full h-full mb-6 flex-col text-[18px] font-Poppins leading-[50.3px] tracking-[1px] text-[#FFFFFF]">
+        <div className="">©2023 AskUs Limited. All rights reserved.</div>
+        <div className="flex flex-col md:flex-row items-center md:gap-3">
+          <Link href="/privacy-policy" className="text-[#FFFFFF]">
+            Privacy Policy
+          </Link>
+          <div className="hidden md:block"> | </div>
+          <Link href="/terms-of-service" className="text-[#FFFFFF]">
+            Terms of Service
+          </Link>
+          <div className="hidden md:block"> | </div>
+          <Link href="/sitemap" className="text-[#FFFFFF]">
+            Sitemap
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,76 +7,87 @@ import Layout from "../components/layout";
 import Nav from "../components/layout/nav";
 import { Button } from "@nextui-org/react";
 import { AiOutlineFundView } from "react-icons/ai";
-import { FaCheckCircle, FaPen } from "react-icons/fa";
+import { FaCheckCircle, FaHome, FaPen } from "react-icons/fa";
 import { MdOutlineLoop } from "react-icons/md";
-import { IoCashOutline } from "react-icons/io5";
+import { IoCashOutline, IoCreateOutline } from "react-icons/io5";
 import { IoMdHelp } from "react-icons/io";
 import { supabase } from "../supabaseClient";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ProfilePage() {
   const auth = useAuth();
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     const fetch = async () => {
-      const authUser = await supabase.auth.getUser()
-      if(!authUser.data.user) return router.push('/login')
-    }
-  fetch()
-  }, [router])
+      const authUser = await supabase.auth.getUser();
+      if (!authUser.data.user) return router.push("/login");
+    };
+    fetch();
+  }, [router]);
 
-  if(!auth.user){
-    return <LoadingScreen />
+  if (!auth.user) {
+    return <LoadingScreen />;
   }
 
   return (
     <>
-    <LoadingScreen />
+      <LoadingScreen />
       <nav className="max-h-[6rem] sticky top-0 z-20 col-[1/-1] row-[1] px-8 py-3 bg-[#F9F9F9] shadow-lg">
         <Nav />
       </nav>
 
-      <div className="relative flex min-h-[calc(100vh-6rem)]">
+      <div className="relative flex min-h-[calc(100vh-6rem)] mb-12">
         <aside className="hidden lg:block w-[25%] max-h-[calc(100vh-6rem)] xl:sticky lg:static top-20 bg-primary text-white">
-          <div className="h-full">
+          <div className="h-full relative">
             <div className="flex flex-col gap-6 mt-8 px-8 w-full">
-              <Button
-                startContent={<AiOutlineFundView />}
-                className="flex justify-start"
-              >
-                Overview
-              </Button>
-              <Button
-                startContent={<MdOutlineLoop />}
-                className="flex justify-start"
-              >
-                Ongiong{" "}
-                <div className="w-6 h-6 grid place-items-center rounded-md bg-primary text-white">
-                  1
-                </div>
-              </Button>
-              <Button
-                startContent={<FaCheckCircle />}
-                className="flex justify-start"
-              >
-                Answered{" "}
-                <div className="w-6 h-6 grid place-items-center rounded-md bg-primary text-white">
-                  12
-                </div>
-              </Button>
-              <Button
-                startContent={<IoCashOutline />}
-                className="flex justify-start"
-              >
-                Subscription / Credit
-              </Button>
-              <Button
-                startContent={<IoMdHelp />}
-                className="flex justify-start"
-              >
-                Help
-              </Button>
+              <Link href={""}>
+                <Button
+                  startContent={<AiOutlineFundView />}
+                  className="flex justify-start"
+                >
+                  Overview
+                </Button>
+              </Link>
+              <Link href={"#ongoing"}>
+                <Button
+                  startContent={<MdOutlineLoop />}
+                  className="flex justify-start"
+                >
+                  Ongiong{" "}
+                  <div className="w-6 h-6 grid place-items-center rounded-md bg-primary text-white">
+                    0
+                  </div>
+                </Button>
+              </Link>
+              <Link href={"#answered"}>
+                <Button
+                  startContent={<FaCheckCircle />}
+                  className="flex justify-start"
+                >
+                  Answered{" "}
+                  <div className="w-6 h-6 grid place-items-center rounded-md bg-primary text-white">
+                    0
+                  </div>
+                </Button>
+              </Link>
+              <Link href={"#subscription"}>
+                <Button
+                  startContent={<IoCashOutline />}
+                  className="flex justify-start"
+                >
+                  Subscription / Credit
+                </Button>
+              </Link>
+              <Link href={"/faq"}>
+                <Button
+                  startContent={<IoMdHelp />}
+                  className="flex justify-start"
+                >
+                  Help
+                </Button>
+              </Link>
             </div>
 
             <div className="absolute bottom-0 py-6 left-0 w-full flex justify-center bg-white">
@@ -90,14 +101,20 @@ export default function ProfilePage() {
         <main className="w-full min-h-[100vh] bg-[#F9F9F9] pb-20">
           <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col items-center justify-center">
-              <div className="mt-16 w-[400px] h-[300px] rounded-xl overflow-hidden border-b-[8px] border-primary shadow-xl flex flex-col justify-center items-center text-[18px] relative">
-                <Button className="bg-transparent absolute top-3 right-2">
+              <div className="md:mt-16 mt-4 w-full max-w-[400px] mx-4 h-[300px] rounded-xl overflow-hidden border-b-[8px] border-primary shadow-xl flex flex-col justify-center items-center text-[18px] relative">
+                <Button
+                  className="bg-transparent absolute top-3 right-2"
+                  isIconOnly
+                >
                   <div className="bg-primary w-10 h-10 rounded-full"></div>
                 </Button>
                 <div className="relative">
                   <div className="w-20 h-20 rounded-full bg-slate-600"></div>
                   <div className="absolute -bottom-2 -right-3">
-                    <Button className="w-7 h-7 min-w-6 min-h-6 rounded-full grid place-items-center">
+                    <Button
+                      className="w-7 h-7 min-w-6 min-h-6 rounded-full grid place-items-center"
+                      isIconOnly
+                    >
                       <FaPen size={12} />
                     </Button>
                   </div>
@@ -121,23 +138,23 @@ export default function ProfilePage() {
               </div>
 
               <div className="flex flex-wrap items-center justify-center gap-10 mt-20">
-                <div className="h-[150px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-[#EBA125] text-center px-8 py-4">
+                <div className="h-[150px] min-w-[215px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-[#EBA125] text-center px-8 py-4">
                   <div className="text-[20px] w-10 h-10 rounded-full bg-black text-white grid place-items-center font-bold">
-                    1
+                    0
                   </div>
                   <div className="text-[#2B2B2B] text-[28px] font-bold font-MontserratBold">
                     Ongoing
                   </div>
                 </div>
-                <div className="h-[150px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-success text-center px-8 py-4">
+                <div className="h-[150px] min-w-[215px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-success text-center px-8 py-4">
                   <div className="text-[20px] w-10 h-10 rounded-full bg-black text-white grid place-items-center font-bold">
-                    12
+                    0
                   </div>
                   <div className="text-[#2B2B2B] text-[28px] font-bold font-MontserratBold">
                     Answered
                   </div>
                 </div>
-                <div className="h-[150px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-[#9005FE] text-center px-8 py-4">
+                <div className="h-[150px] min-w-[215px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-[#9005FE] text-center px-8 py-4">
                   <div className="text-[20px] w-10 h-10 rounded-full bg-black text-white grid place-items-center font-bold">
                     {auth.user?.credit || 0}
                   </div>
@@ -145,7 +162,7 @@ export default function ProfilePage() {
                     Credits
                   </div>
                 </div>
-                <div className="h-[150px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-primary text-center px-8 py-4">
+                <div className="h-[150px] min-w-[215px] flex flex-col justify-between bg-white shadow-xl rounded-md border-b-[5px] border-primary text-center px-8 py-4">
                   <div className="text-[20px] font-bold">
                     {auth.user?.isSubscribed ? "Active" : "Unsubscribed"}
                   </div>
@@ -158,6 +175,77 @@ export default function ProfilePage() {
           </div>
         </main>
       </div>
+
+      <div className="fixed z-[999999] bottom-20 right-2">
+        <Button className="bg-primary text-white w-[50px] h-[50px]" isIconOnly>
+          <IoCreateOutline size={24} />
+        </Button>
+      </div>
+      <MobileFooter />
     </>
   );
 }
+
+const MobileFooter = () => {
+  return (
+    <div className="lg:hidden fixed bottom-0 left-0 z-50 w-full bg-white py-3 px-3">
+      <div className="flex items-center justify-center gap-5 md:gap-8">
+        <Link href="">
+          <Button
+            isIconOnly
+            variant="faded"
+            aria-label="Overview"
+            title="Overview"
+            className="bg-primary shadow-none border-none"
+          >
+            <FaHome className="text-white" />
+          </Button>
+        </Link>
+        <Link href="#ongoing">
+          <Button
+            isIconOnly
+            variant="faded"
+            aria-label="Ongoing"
+            title="Ongoing"
+            className="bg-primary shadow-none border-none"
+          >
+            <MdOutlineLoop className="text-white" />
+          </Button>
+        </Link>
+        <Link href="#answered">
+          <Button
+            isIconOnly
+            variant="faded"
+            aria-label="Answered"
+            title="Answered"
+            className="bg-primary shadow-none border-none"
+          >
+            <FaCheckCircle className="text-white" />
+          </Button>
+        </Link>
+        <Link href="#subscription">
+          <Button
+            isIconOnly
+            variant="faded"
+            aria-label="Subscription / Credits"
+            title="Subscription / Credits"
+            className="bg-primary shadow-none border-none"
+          >
+            <IoCashOutline className="text-white" />
+          </Button>
+        </Link>
+        <Link href="/faq">
+          <Button
+            isIconOnly
+            variant="faded"
+            aria-label="Help"
+            title="Help"
+            className="bg-primary shadow-none border-none"
+          >
+            <IoMdHelp className="text-white" />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
