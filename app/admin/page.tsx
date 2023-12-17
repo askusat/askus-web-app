a968e0d1ef0bc7041194c847d4306d518bd0fb17
innocentdavid <paulinnocent04@gmail.com>
2023-12-17 15:24:40 +0100

    x

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div className="flex items-center   justify-center w-full h-full ">
+      {/* Left Side */}
+      <div className="max-w-[25%] w-full h-screen   bg-primary">ee</div>
+      {/* Right Side */}
+      <div className="w-[72%] h-screen px-10 bg-white">eeee</div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
