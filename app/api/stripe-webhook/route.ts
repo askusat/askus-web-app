cd8260213e44a9e745e8f0f97eea5fcdc09af93a
innocentdavid <paulinnocent04@gmail.com>
2024-01-08 14:23:38 +0100

    v

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -2,6 +2,7 @@ import { supabase } from "@/app/supabaseClient";
 import { headers } from "next/headers";
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
+import { User } from "@/types";
 
 const SK = process.env.STRIPE_SECRET_KEY;
 const stripe = new Stripe(SK || "");
@@ -44,9 +45,10 @@ export async function POST(request: Request) {
       .update({
         stripeSubscriptionId: subscription.id,
         subscription_status: subscription.status,
-        isSubscribed: subscription.status === "active" ? true : false,
+        isSubscribed: (subscription.status === "active" || subscription.status === "trialing") ? true : false,
         updatedAt: new Date(),
       })
+      // } as Partial<User>)
       .eq("stripeCustomerId", subscription.customer);
 
     if (error) {
