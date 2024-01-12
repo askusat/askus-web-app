adfa54ad5ef104988296302adc905bc297ac8af1
innocentdavid <paulinnocent04@gmail.com>
2024-01-12 19:45:02 +0100

    added fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import CreateChat from "./components/chat/createChat";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const APP_NAME = "ASKUS";
@@ -49,6 +50,9 @@ export const metadata: Metadata = {
     },
     description: APP_DESCRIPTION,
   },
+  other: {
+    "facebook-domain-verification": "794mkkv0g1djy10teis3fuf98fqqna",
+  },
 };
 
 export const viewport: Viewport = {
@@ -62,6 +66,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '905236264437291');
+fbq('track', 'PageView');
+`,
+        }}
+      />
+
       <body
         className={`${inter.className} font-MontserratRegular bg-white text-black`}
       >
