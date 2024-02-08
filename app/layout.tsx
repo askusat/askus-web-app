3d495efe7a1a256700953b9c4c8c917a52298296
innocentdavid <paulinnocent04@gmail.com>
2024-02-08 18:04:44 +0100

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,8 @@ export const viewport: Viewport = {
 //   };
 // }
 
+// GTM-T9GXXQG6
+
 export default function RootLayout({
   children,
 }: {
@@ -126,14 +128,14 @@ fbq('track', 'PageView');
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T9GXXQG6');
+})(window,document,'script','dataLayer','GTM-TTTXJ7HH');
         `,
         }}
       />
 
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-T9GXXQG6"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TTTXJ7HH"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
